25e5ef5a26027cff7447b55cb1d6ef1e3f478ecd
derrick b <dbozich1@gmail.com>
2018-06-05 02:43:09 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -8,9 +8,19 @@ kickstarter = Nokogiri::HTML(html)
 
 def create_project_hash
   # write your code here
-  
+
 end
 kickstarter.css("li.project.grid_4").first
 
 # create_project_hash
 binding.pry
+
+<p class="bbcard_blurb">
+Four young composers join forces with an indie orchestra to present this musical reflection on a literary masterpiece.
+</p>
+
+# projects: kickstarter.css("li.project.grid_4")
+# title: project.css("h2.bbcard_name strong a").text
+# image link: project.css("div.project-thumbnail a img").attribute("src").value
+# description: project.css("p.bbcard_blurb").text
+
